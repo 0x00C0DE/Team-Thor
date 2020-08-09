c1723b7ae57f9da5057fdfbc984583c119910316
princeri1 <59859763+princeri1@users.noreply.github.com>
2020-08-09 13:47:09 -0700

    Update .dblogin.php
    added commenting

--- .dblogin.php
+++ .dblogin.php
@@ -1,5 +1,5 @@
 <?php
-
+//Function filters input
 function clean_input($input) {
    $input = trim($input);
    $input = stripslashes($input);
@@ -7,20 +7,20 @@ function clean_input($input) {
    return $input;
 
 }
-
+//function connects to database unless error occurs
 function dbconnect() {
    $hostname = 'classmysql.engr.oregonstate.edu';
    $username = 'cs361_haggerto';
    $password = '=R:3.s*\'&MwVU-Qcbv#5)c^L)(]["~a}V%%';
 
    $con = mysqli_connect($hostname, $username, $password, $username);
-
+//If connection cant be made it fails
    if (!$con) {
       die("Connection failed" . mysqli_connect_error());
    }
    return $con;
 }
-
+//Closes database connection
 function dbclose($con) {
    mysqli_close($con);
 }
